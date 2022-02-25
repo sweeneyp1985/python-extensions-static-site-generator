1497bea43c084c4a766cc060791f1a55656193e9
“I558398” <paul.sweeney@sap.com>
2022-02-25 23:31:00 +0000

    add in event function

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -11,5 +11,5 @@ def register(hook, order=0):
 def event(hook, *args):
   for order in sorted(_callbacks.get(hook, {})):
       for func in _callbacks[hook][order]:
-          value = func(value, *args)
+          value = func(*args)
   return value
