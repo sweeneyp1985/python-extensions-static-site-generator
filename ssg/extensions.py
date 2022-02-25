b51866b2a697b9d82c19fc842a3c184e9f16c8ac
“I558398” <paul.sweeney@sap.com>
2022-02-25 22:44:54 +0000

    spell correction and for loop addition

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -2,7 +2,17 @@ import sys
 import importlib
 from pathlib import Path
 
-def load_module(dictionary,name):
-    sys.path.insert(0,dictionary)
+def load_module(directory,name):
+    sys.path.insert(0, directory)
     importlib.import_module(name)
     sys.path.pop(0)
+
+
+def load_directory(dictionary):
+    for path in directory.rglob(.*py):
+        load_module(directory.as_posix(), path.stem)
+
+
+def load_bundled():
+    dictionary = Path(__file__).parent / "extensions"
+    load_dictionary(dictionary)
