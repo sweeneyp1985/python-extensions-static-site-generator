44d5ef65e3cb2c4e9adaf185295e9f1a90c3d6b9
“I558398” <paul.sweeney@sap.com>
2022-02-25 23:56:45 +0000

    module3

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -3,5 +3,5 @@ from ssg import hooks, parsers
 files = []
 
 @hooks.register("collect_files")
-def collect_files(source,site_parsers):
-valid = lambda p: not isinstance(p, parsers.ResoursceParser)
+def collect_files(source, site_parsers):
+    valid = lambda p: not isinstance(p, parsers.ResoursceParser)
