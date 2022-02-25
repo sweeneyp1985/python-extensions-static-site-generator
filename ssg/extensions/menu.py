86b3912d1550755ff10d7cd89fa4f56e0672bc03
“I558398” <paul.sweeney@sap.com>
2022-02-25 23:58:50 +0000

    module3

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -4,4 +4,8 @@ files = []
 
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
-    valid = lambda p: not isinstance(p, parsers.ResoursceParser)
+    valid = lambda p: not isinstance(p, parsers.ResourceParser)
+    for path in source.rglob("*"):
+        for parser in list(filter(valid, site_parsers)):
+            if parser.valid_file_ext(path.suffix):
+                files.append(path)
