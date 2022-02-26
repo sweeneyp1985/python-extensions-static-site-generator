0c2ec80da569d25f4b674c0a53fa2606061cc2ec
“I558398” <paul.sweeney@sap.com>
2022-02-26 00:06:26 +0000

    module3

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -9,3 +9,10 @@ def collect_files(source, site_parsers):
         for parser in list(filter(valid, site_parsers)):
             if parser.valid_file_ext(path.suffix):
                 files.append(path)
+
+
+@hooks.register("generate_menu")
+def generate_menu(html, ext):
+    template = '<li><a href="{}{}">{}</a></li>'
+    menu_item = lambda name, ext: template.format(name, ext, name.title())
+    menu = "\n".join([menu_item(path.stem, ext) for path in files ])
