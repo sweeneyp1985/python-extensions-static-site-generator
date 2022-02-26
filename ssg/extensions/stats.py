0e24d61f28a994e419fb49924c8566746668d071
“I558398” <paul.sweeney@sap.com>
2022-02-26 00:38:39 +0000

    module4

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -1,7 +1,26 @@
-from ssg import hooks
 import time
+from ssg import hooks
 
 start_time = None
 total_written = 0
 
-def start_build("start_build):
+
+@hooks.register("start_build")
+def start_build():
+    global start_time
+    start_time = time.time()
+
+
+
+@hooks.register("written")
+def written():
+    global start_time
+    start_time = time.time()
+
+
+@hooks.register("stats")
+def stats():
+    final_time = time.time() - start_time
+    average = final_time / total_written if total_written else 0
+    report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
+    print(report.format(total_written, final_time, average))
